a0d63dcb3057311ee12c5b30c25e5b455805d621
boodera <namish.chaturvedi@goodera.com>
2018-07-20 17:41:28 +0530

    Update update-ip-address-in-sg.sh

--- src/update-ip-address-in-sg.sh
+++ src/update-ip-address-in-sg.sh
@@ -10,4 +10,4 @@ fi
 REGION_ID="ap-southeast-1"
 GROUP_ID="sg-aabbccdd"
 
-aws ec2 authorize-security-group-ingress --region $REGION_ID --group-id $GROUP_ID --ip-permissions '[{"IpProtocol": "ssh", "FromPort": 22, "ToPort": 22, "IpRanges": [{"CidrIp": \""$IPADDRESS"\", "Description": "SSH access from My home"}]}]'
+aws ec2 update-security-group-rule-descriptions-ingress --region $REGION_ID --group-id $GROUP_ID --ip-permissions '[{"IpProtocol": "ssh", "FromPort": 22, "ToPort": 22, "IpRanges": [{"CidrIp": \""$IPADDRESS"\", "Description": "SSH access from My home"}]}]'
