6387eed5092ef01539915bbe14ea10e7c47d3738
boodera <namish.chaturvedi@goodera.com>
2018-07-20 17:39:11 +0530

    Create update-ip-address-in-sg.sh

---
+++ src/update-ip-address-in-sg.sh
@@ -0,0 +1,13 @@
+EXTRACTIP=$(host myip.opendns.com resolver1.opendns.com | grep " has address " | cut -d' ' -f4)
+
+if [ -z "$EXTRACTIP" ]
+then
+        echo "You are not connected to the internet."
+        exit 1
+else
+        IPADDRESS=$EXTRACTIP/32
+fi
+REGION_ID="ap-southeast-1"
+GROUP_ID="sg-aabbccdd"
+
+aws ec2 authorize-security-group-ingress --region $REGION_ID --group-id $GROUP_ID --ip-permissions '[{"IpProtocol": "ssh", "FromPort": 22, "ToPort": 22, "IpRanges": [{"CidrIp": \""$IPADDRESS"\", "Description": "SSH access from My home"}]}]'
